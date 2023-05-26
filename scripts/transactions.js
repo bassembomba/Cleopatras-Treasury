49fc99fb912997a296ed2712f24304da0e574027
bassem ehab <bassemelgen500@gmail.com>
2023-05-26 18:25:14 +0300

    navbar + new transactions page

---
+++ scripts/transactions.js
@@ -0,0 +1,23 @@
+const toggle = document.getElementById("toggle");
+const bankTransactionsTable = document.querySelector(".bank-transactions");
+const creditCardTransactionsTable = document.querySelector(
+  ".credit-card-transactions"
+);
+
+toggle.addEventListener("change", () => {
+  if (toggle.checked) {
+    bankTransactionsTable.style.display = "none";
+    creditCardTransactionsTable.style.display = "table";
+  } else {
+    bankTransactionsTable.style.display = "table";
+    creditCardTransactionsTable.style.display = "none";
+  }
+});
+const sidebarToggle = document.getElementById("sidebarToggle");
+const sidebar = document.querySelector(".sidebar");
+const content = document.querySelector(".content");
+
+sidebarToggle.addEventListener("click", () => {
+  sidebar.classList.toggle("collapsed");
+  content.classList.toggle("expanded");
+});
